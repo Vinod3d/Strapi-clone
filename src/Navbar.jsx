8432ac36cf4a24961e352af4d98c4f68732d7697
Vinod Chandra <vinodchandra979@gmail.com>
2023-09-29 20:29:45 +0530

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useGlobalContext } from './Context'
 import { FaBars } from 'react-icons/fa';
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
     const {openSidebar} = useGlobalContext();
@@ -11,6 +12,7 @@ const Navbar = () => {
         <button className='toggle-btn' onClick={openSidebar}>
             <FaBars/>
         </button>
+        <NavLinks/>
       </div>
     </nav>
   )
